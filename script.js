aeea7052cb7b85e3f46281ff9db3e3214fa2e5d9
herasymenko-d <shiningxors@gmail.com>
2024-05-09 17:56:00 +0300

    add styles

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
 let languageOptions;
+let currentLanguaage; 
 
 function changeLanguage(language) {
   loadTranslation(language);
-
+  currentLanguaage = language;
+fetchReviews()
   languageOptions.forEach(option => {
     if (option.textContent.trim() === language) {
+      console.log("languageOptions", language)
+
+      fetchReviews();
       // option.classList.add('font-bold');
       option.classList.add('underline');
     } else {
@@ -14,6 +19,34 @@ function changeLanguage(language) {
     }
   });
 }
+
+const apiKey = 'AIzaSyDo2OlY6CXwLC6OZCiaGdYlLRO9KFCunYk';
+const placeId = 'ChIJMxXAk0kXD0cR9hFDyIYkvxU'; // Ваш place_id
+const language = currentLanguaage; // Код языка для украинского
+console.log("currentLanguaage", currentLanguaage)
+async function fetchReviews() {
+  try {
+    // Формуємо URL запиту до Places Details API
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=reviews&language=${language}&key=${apiKey}`);
+    const data = await response.json();
+    console.log("response", response )
+    if (data.status === 'OK') {
+      const reviews = data.result.reviews; // Отримуємо масив відгуків
+      console.log('Відгуки про місце:', reviews);
+      return reviews;
+    } else {
+      console.error('Помилка при отриманні відгуків:', data.status);
+    }
+  } catch (error) {
+    console.error('Помилка при отриманні відгуків:', error);
+  }
+}
+
+// Виклик функції для отримання відгуків
+fetchReviews();
+
+
+
 
 function loadTranslation(language) {
   fetch(`locales/${language}.json`)
@@ -44,3 +77,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
   changeLanguage(defaultLanguage);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const readMoreBtns = document.querySelectorAll('.read-more-btn');
+
+  readMoreBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      const textContainers = document.querySelectorAll('.col-span-8 .hidden');
+      textContainers.forEach(function(container) {
+        container.classList.toggle('hidden');
+      });
+      if (btn.textContent === 'czytaj więcej') {
+        btn.textContent = 'ukryj';
+      } else {
+        btn.textContent = 'czytaj więcej';
+
+      }
+    });
+  });
+});
+
+
+
+
+
